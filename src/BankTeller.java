f910bc1fb6a0c16f97aa5d2fdeac2b97b4ab95c1
aalvinn03 <94874703+aalvinn03@users.noreply.github.com>
2022-02-16 17:36:28 -0500

    Finished profile class

--- src/BankTeller.java
+++ src/BankTeller.java
@@ -1,5 +1,5 @@
 public class BankTeller {
-    
+
     public void run(){
 
     }
--- src/Profile.java
+++ src/Profile.java
@@ -2,4 +2,26 @@ public class Profile {
     private String fname;
     private String lname;
     private Date dob;
+
+    public Profile(String fname, String lname, Date dob){
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+    }
+
+    public String toString(){
+        return this.fname + " " + this.lname + "DOB: " + this.dob.toString();
+    }
+
+    public boolean equals(Profile profile){
+        if(this.fname.equals(profile.fname)){
+            if(this.lname.equals(profile.lname)){
+                if(this.dob.compareTo(profile.dob) == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
