30a23747bf5fbff2d01aea8dac5bc914294a232c
aalvinn03 <94874703+aalvinn03@users.noreply.github.com>
2022-02-28 19:15:36 -0500

    push v3

--- src/AccountDatabaseTest.java
+++ src/AccountDatabaseTest.java
@@ -5,7 +5,16 @@ import static org.junit.Assert.*;
 public class AccountDatabaseTest {
 
     @Test
+    // Test 1: Open a Savings account normally
+    // Test 2: Open a Checking account normally
+    // Test 3: Open a CollegeChecking account normally
+    // Test 4: Open a Money Market Savings account normally
+    // Test 5: Reopen a closed account
+    // Test 6: Open an account when there is already the same type in database
+    // Test 7: Open a checking account when there is already a college checking account in the database
+    // Test 8: Open a college checking account when there is already a checking account in the database
     public void open() {
+
     }
 
     // Test 1: Close an account normally
